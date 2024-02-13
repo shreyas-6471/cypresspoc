c2b5782b05593c8d38cd440170f5e3f3b5bc05b9
shreyas-6471 <37994152+shreyas-6471@users.noreply.github.com>
2024-02-13 18:31:31 -0500

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "kd3hrw",
   defaultCommandTimeout:6000,
+  scripts:{
+    test:"npx cypress run",
+    testDashboard:"npx cypress run --record --key 805cbb9f-1cd4-43fa-b7fa-6d23951257c6",
+    
+  },
   env:{
     url:"https://rahulshettyacademy.com"
   },
