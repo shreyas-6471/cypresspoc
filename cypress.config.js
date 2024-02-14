35df53afa9c3cab575716c50bb63ea7c1906b9e9
shreyas-6471 <37994152+shreyas-6471@users.noreply.github.com>
2024-02-13 20:07:03 -0500

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,28 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 module.exports = defineConfig({
   projectId: "kd3hrw",
   defaultCommandTimeout:6000,
   scripts:{
     test:"npx cypress run",
     testDashboard:"npx cypress run --record --key 805cbb9f-1cd4-43fa-b7fa-6d23951257c6",
-    
+
   },
   env:{
     url:"https://rahulshettyacademy.com"
   },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-
-    },
-    specPattern:'cypress/integration/examples/*.js'
+    setupNodeEvents,
+    specPattern:'cypress/integration/examples/BDD/*.feature'
   },
 });
